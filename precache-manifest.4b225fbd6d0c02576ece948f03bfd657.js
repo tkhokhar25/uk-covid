50257d1d4e43deba99a37b068e82fbaf7db03104
tkhokhar25 <tkhokhar25@gmail.com>
2020-04-08 16:39:31 +0530

    Updates

---
+++ precache-manifest.4b225fbd6d0c02576ece948f03bfd657.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa41b505ef21d2f15655289350360cd",
+    "url": "/uk-covid/index.html"
+  },
+  {
+    "revision": "5ef61b355731d253961e",
+    "url": "/uk-covid/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e485544c3badfcc94246",
+    "url": "/uk-covid/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5ef61b355731d253961e",
+    "url": "/uk-covid/static/js/2.9ccf90f6.chunk.js"
+  },
+  {
+    "revision": "4f4d8ec2d74796ff8c46021fef29eb82",
+    "url": "/uk-covid/static/js/2.9ccf90f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e485544c3badfcc94246",
+    "url": "/uk-covid/static/js/main.d5f2bbfa.chunk.js"
+  },
+  {
+    "revision": "ff574a3da2b2b9290b3b",
+    "url": "/uk-covid/static/js/runtime-main.8e8543b4.js"
+  }
+]);
